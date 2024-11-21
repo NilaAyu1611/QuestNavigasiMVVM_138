23129aa1ff9581fcfbd8e8155542c7192c77648e
NilaAyu1611 <nilaayucw@gmail.com>
2024-11-21 20:03:51 +0700

    data class DataSiswa  digunakan untuk menyimpan data dengan properti (nim,nama, alamat,dll)

--- app/src/main/java/com/example/prak6/model/DataSiswa.kt
+++ app/src/main/java/com/example/prak6/model/DataSiswa.kt
@@ -2,7 +2,8 @@ package com.example.prak6.model
 
 import android.provider.ContactsContract.CommonDataKinds.Email
 
-data class DataSiswa(
+data class DataSiswa(                                   // Menyimpan data dengan properti
+    val nim: String = "",
     val nama :String = "",
     val alamat :String = "",
     val gender :String = "",
